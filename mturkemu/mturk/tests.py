3d59826fbcc705b759dcf84c152e88ec5229fb14
Carl Allendorph <callendorph@gmail.com>
2017-04-11 11:13:54 -0700

    Added initial unit test for the API. This uses a liveserver test case to allow use of the boto3 client directly

---
+++ mturkemu/mturk/tests.py
@@ -0,0 +1,11 @@
+# File: mturk/tests.py
+# Author: Carl Allendorph
+#
+# Description:
+#   This file contains a list of all the unit tests that are
+# run when the `./manage.py test` command is run.
+#
+
+from django.test import TestCase
+
+from mturk.testsuite.api import *
