8d2938574c05f1675328b07d5ccf7876eac8daa3
Carl Allendorph <callendorph@gmail.com>
2017-04-11 11:13:12 -0700

    Added new data migration for the mturk app so that the qualification data get created at migration time

--- mturkemu/tools/remove_migrations.sh
+++ mturkemu/tools/remove_migrations.sh
@@ -5,13 +5,15 @@ echo ">> Deleting old migrations"
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 
-
 # Optional
 echo ">> Deleting database"
 find . -name "db.sqlite3" -delete
 
 echo ">> Running manage.py makemigrations"
 python manage.py makemigrations
+
+echo ">> Adding Data Initialization Migration"
+ln -s ../datamigrations/0002_init_data.py mturk/migrations/
 
 echo ">> Running manage.py migrate"
 python manage.py migrate
