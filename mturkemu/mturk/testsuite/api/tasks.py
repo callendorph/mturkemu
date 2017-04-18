78e5b8fbdd9dcd2117821109cd87a3bbb960f797
Carl Allendorph <callendorph@gmail.com>
2017-04-18 16:28:34 -0700

    Added more unit tests for the tasks and assignments portion of the API

--- mturkemu/mturk/testsuite/api/tasks.py
+++ mturkemu/mturk/testsuite/api/tasks.py
@@ -199,6 +199,11 @@ class TaskTests(RequesterLiveTestCase):
 
 
     def create_workers(self):
+        """
+        Utility method for creating users with some prepared qualifications
+        associated with those workers.
+        Use the 'self.actors' object to access the workers created.
+        """
         self.actors = []
         usernames = ["test2", "test3", "test4"]
         vals = [ 10, 20, 30 ]
@@ -1045,3 +1050,521 @@ class TaskTests(RequesterLiveTestCase):
         self.assertEqual( obj["NumberOfAssignmentsAvailable"], 0 )
         self.assertEqual( obj["NumberOfAssignmentsCompleted"], 2)
         self.assertEqual( obj["HITStatus"], "Reviewing" )
+
+
+
+    def test_create_task(self):
+        """
+        In this test we will create a task without a task type. We will
+        also test task creation with qualification requirements
+        """
+        startTime = timezone.now()
+        self.create_quals()
+        self.create_workers()
+
+        maxAssigns = 3
+        lifetime = 10000
+        assignDur = 1000
+        reward = "0.13"
+        title = "Create Task with Quals"
+        desc = "Little bit of sugar"
+        annot = "asdf"
+        question = load_quesform(2)
+
+        resp = self.client.create_hit(
+            MaxAssignments = maxAssigns,
+            LifetimeInSeconds = lifetime,
+            AssignmentDurationInSeconds = assignDur,
+            Reward = reward,
+            Title = title,
+            Description = desc,
+            Question = question,
+            RequesterAnnotation = annot,
+            QualificationRequirements=[
+                {
+                    "QualificationTypeId" : self.quals[0],
+                    "Comparator" : "GreaterThan",
+                    "IntegerValues" : [ 20 ],
+                    "RequiredToPreview" : False
+                }
+            ],
+            UniqueRequestToken="ureuw"
+        )
+        self.is_ok(resp)
+
+        obj = resp["HIT"]
+
+        self.assertTrue( len(obj["HITTypeId"]) > 0 )
+
+        self.assertTrue( obj["CreationTime"] > startTime )
+        self.assertEqual( obj["Title"], title )
+        self.assertEqual( obj["Description"], desc )
+        self.assertEqual( obj["Question"], question )
+        self.assertEqual( obj["HITStatus"], "Assignable" )
+        self.assertEqual( obj["MaxAssignments"], maxAssigns )
+        self.assertEqual( obj["Reward"], reward )
+        self.assertTrue( obj["AutoApprovalDelayInSeconds"] > 0 )
+        self.assertTrue( obj["Expiration"] > startTime )
+        self.assertTrue( obj["Expiration"] > obj["CreationTime"] )
+        self.assertEqual( obj["AssignmentDurationInSeconds"], assignDur)
+        self.assertEqual( obj["RequesterAnnotation"], annot )
+
+        qualreqs = obj["QualificationRequirements"]
+        self.assertEqual( len(qualreqs), 1 )
+
+        qualreq = qualreqs[0]
+        self.assertEqual( qualreq["QualificationTypeId"], self.quals[0] )
+        self.assertEqual( qualreq["Comparator"], "GreaterThan" )
+        self.assertEqual( qualreq["IntegerValues"], [ 20 ] )
+        self.assertEqual( qualreq["RequiredToPreview"], False )
+
+        with self.assertRaises(KeyError):
+            qualreq["LocaleValues"]
+
+        taskId = obj["HITId"]
+
+        task = Task.objects.get( aws_id = taskId )
+
+        avail, pend, complete, submitted = task.compute_assignment_stats()
+        self.assertEqual( avail, maxAssigns )
+        self.assertEqual( pend, 0 )
+        self.assertEqual( complete, 0 )
+        self.assertEqual( submitted, 0 )
+
+        # Now we will try to access the task with a worker who
+        # does not have the necessary qualifications.
+        with self.assertRaises(TaskPrereqError):
+            assignment = self.actors[0].accept_task( task )
+        with self.assertRaises(TaskPrereqError):
+            assignment = self.actors[1].accept_task( task )
+
+        # Confirm the HIT's stats haven't change
+        task.refresh_from_db()
+        avail, pend, complete, submitted = task.compute_assignment_stats()
+        self.assertEqual( avail, maxAssigns )
+        self.assertEqual( pend, 0 )
+        self.assertEqual( complete, 0 )
+        self.assertEqual( submitted, 0 )
+
+        # Try with actor that meets the qualification
+        assignment = self.actors[2].accept_task( task )
+
+        task.refresh_from_db()
+        avail, pend, complete, submitted = task.compute_assignment_stats()
+        self.assertEqual( avail, maxAssigns - 1 )
+        self.assertEqual( pend, 1 )
+        self.assertEqual( complete, 0 )
+        self.assertEqual( submitted, 0 )
+
+        self.assertEqual( assignment.task, task )
+        self.assertEqual( assignment.worker.aws_id, self.actors[2].worker.aws_id )
+
+
+    def test_delete_task(self):
+        """
+        Test the creation and deletion of a task
+        """
+
+        startTime = timezone.now()
+        self.create_quals()
+        self.create_workers()
+
+        maxAssigns = 1
+        lifetime = 10000
+        assignDur = 1000
+        reward = "0.13"
+        title = "Create Task with Quals"
+        desc = "Little bit of sugar"
+        annot = "asdf"
+        question = load_quesform(2)
+
+        resp = self.client.create_hit(
+            MaxAssignments = maxAssigns,
+            LifetimeInSeconds = lifetime,
+            AssignmentDurationInSeconds = assignDur,
+            Reward = reward,
+            Title = title,
+            Description = desc,
+            Question = question,
+            RequesterAnnotation = annot,
+            UniqueRequestToken="ureuw"
+        )
+        self.is_ok(resp)
+
+        taskId = resp["HIT"]["HITId"]
+
+        # We should not be able to delete a task until it
+        # has transitioned to the reviewable state (or later) and
+        # all assignments are approved/rejected.
+        RequestError = self.client._load_exceptions().RequestError
+        with self.assertRaises(RequestError):
+            resp = self.client.delete_hit(HITId = taskId)
+
+
+        resp = self.client.get_hit(HITId = taskId)
+        self.is_ok(resp)
+
+        taskObj = resp["HIT"]
+        self.assertEqual( taskObj["HITStatus"], "Assignable" )
+
+        # Accept and complete an assignment and then retest the
+        # delete method
+        task = Task.objects.get( aws_id = taskId )
+        assignment = self.actors[0].accept_task(task)
+
+        resp = self.client.get_hit(HITId = taskId)
+        self.is_ok(resp)
+
+        taskObj = resp["HIT"]
+        self.assertEqual( taskObj["HITStatus"], "Unassignable" )
+
+        with self.assertRaises(RequestError):
+            resp = self.client.delete_hit(HITId = taskId)
+
+        data = {
+            "favorite" : ["blue"],
+            "acceptible" : ["red", "blue"]
+        }
+        self.actors[0].complete_assignment( assignment, data )
+
+        resp = self.client.get_hit(HITId = taskId)
+        self.is_ok(resp)
+
+        taskObj = resp["HIT"]
+        self.assertEqual( taskObj["HITStatus"], "Reviewable" )
+
+        # State is now reviewable but we haven't approved/rejected
+        #   the assignment so we should still not be able to
+        #   delete the task
+        with self.assertRaises(RequestError):
+            resp = self.client.delete_hit(HITId = taskId)
+
+        resp = self.client.approve_assignment(
+            AssignmentId = assignment.aws_id,
+            RequesterFeedback = "Good Job"
+        )
+        self.is_ok(resp)
+
+        # Check status
+        resp = self.client.get_hit(HITId = taskId)
+        self.is_ok(resp)
+
+        taskObj = resp["HIT"]
+        self.assertEqual( taskObj["HITStatus"], "Reviewable" )
+        self.assertEqual( taskObj["NumberOfAssignmentsCompleted"], 1 )
+
+        # Now finally we should be able to delete the Task
+
+        # Test that another requester can't delete our Task
+        requester = self.create_new_client("req1")
+        with self.assertRaises(RequestError):
+            resp = requester.delete_hit(HITId = taskId)
+
+        resp = self.client.delete_hit(HITId = taskId)
+        self.is_ok(resp)
+
+        with self.assertRaises(RequestError):
+            self.client.get_hit(HITId = taskId)
+
+
+    def test_list_hits(self):
+        """
+        Test the "ListHITs" method
+        """
+
+        # First let's create a bunch of a tasks that we
+        # can look through
+        numTasksToCreate = 10
+
+        maxAssigns = 1
+        lifetime = 10000
+        assignDur = 1000
+        reward = "0.13"
+        title = "Create Task with Quals"
+        desc = "Little bit of sugar"
+        annot = "asdf"
+        question = load_quesform(2)
+
+        taskIds = []
+
+        for i in range(0, numTasksToCreate):
+            resp = self.client.create_hit(
+                MaxAssignments = maxAssigns,
+                LifetimeInSeconds = lifetime,
+                AssignmentDurationInSeconds = assignDur,
+                Reward = reward,
+                Title = title,
+                Description = desc,
+                Question = question,
+                RequesterAnnotation = annot,
+                UniqueRequestToken="ureuw" + str(i)
+            )
+            self.is_ok(resp)
+
+            taskObj = resp["HIT"]
+            taskIds.append( taskObj["HITId"] )
+
+
+        # List out the tasks
+        obsTaskIds = []
+
+        # Get a chunk of HITs
+        resp = self.client.list_hits( MaxResults = 5 )
+        self.is_ok(resp)
+
+        numResults = resp["NumResults"]
+        self.assertEqual(numResults, 5 )
+        tasks = resp["HITs"]
+        self.assertEqual( len(tasks), 5 )
+
+        for task in tasks:
+            obsTaskIds.append( task["HITId"] )
+
+        nextToken = resp["NextToken"]
+        self.assertTrue( len(nextToken) > 0 )
+
+        # Get the next chunk
+        resp = self.client.list_hits(MaxResults = 5, NextToken = nextToken )
+        self.is_ok(resp)
+
+        numResults = resp["NumResults"]
+        self.assertEqual( numResults, 5 )
+        tasks = resp["HITs"]
+        self.assertEqual( len(tasks), 5 )
+
+        for task in tasks:
+            obsTaskIds.append( task["HITId"] )
+
+        nextToken = resp["NextToken"]
+        self.assertTrue( len(nextToken) > 0 )
+
+        # Get final chunk - should be zero len
+        resp = self.client.list_hits(MaxResults = 5, NextToken = nextToken )
+        self.is_ok(resp)
+
+        numResults = resp["NumResults"]
+        self.assertEqual( numResults, 0 )
+        tasks = resp["HITs"]
+        self.assertEqual( len(tasks), 0 )
+
+
+        # Compare the known task ids with the observed task ids
+        self.assertEqual( set(obsTaskIds), set(taskIds) )
+
+        # Create another requester and attempt to look
+        # for tasks - we should not be able to see main user's
+        # tasks.
+
+        requester = self.create_new_client("req1")
+
+        resp = requester.list_hits(MaxResults = 5)
+        self.is_ok(resp)
+
+        numResults = resp["NumResults"]
+        self.assertEqual( numResults, 0 )
+
+
+    def test_return_assignment(self):
+        """
+        This test will check the "return" assignment behavior for
+        a worker.
+        """
+        startTime = timezone.now()
+        self.create_quals()
+        self.create_workers()
+
+        maxAssigns = 2
+        lifetime = 10000
+        assignDur = 1000
+        reward = "0.13"
+        title = "Create Task with Quals"
+        desc = "Little bit of sugar"
+        annot = "asdf"
+        question = load_quesform(2)
+
+        resp = self.client.create_hit(
+            MaxAssignments = maxAssigns,
+            LifetimeInSeconds = lifetime,
+            AssignmentDurationInSeconds = assignDur,
+            Reward = reward,
+            Title = title,
+            Description = desc,
+            Question = question,
+            RequesterAnnotation = annot,
+            UniqueRequestToken="ureuw"
+        )
+        self.is_ok(resp)
+
+        taskId = resp["HIT"]["HITId"]
+        task = Task.objects.get(aws_id = taskId)
+
+        self.actors[0].worker.refresh_from_db()
+        self.assertEqual( self.actors[0].worker.returned_hits, 0 )
+
+        avail, pend, complete, submitted = task.compute_assignment_stats()
+        self.assertEqual( avail, maxAssigns )
+        self.assertEqual( pend, 0 )
+        self.assertEqual( complete, 0 )
+        self.assertEqual( submitted, 0 )
+
+        assignment = self.actors[0].accept_task(task)
+
+        task.refresh_from_db()
+        avail, pend, complete, submitted = task.compute_assignment_stats()
+        self.assertEqual( avail, maxAssigns - 1 )
+        self.assertEqual( pend, 1 )
+        self.assertEqual( complete, 0 )
+        self.assertEqual( submitted, 0 )
+
+        # Check if another worker can return the assignment
+        # accepted by the first user
+        with self.assertRaises(Assignment.DoesNotExist):
+            self.actors[1].return_task(task)
+
+        # return the task by the actual worker
+        self.actors[0].return_task(task)
+
+        task.refresh_from_db()
+        avail, pend, complete, submitted = task.compute_assignment_stats()
+        self.assertEqual( avail, maxAssigns )
+        self.assertEqual( pend, 0 )
+        self.assertEqual( complete, 0 )
+        self.assertEqual( submitted, 0 )
+
+        # Check that the worker's stats have updated.
+        self.actors[0].worker.refresh_from_db()
+        self.assertEqual( self.actors[0].worker.returned_hits, 1 )
+
+    def test_award_bonus(self):
+        """
+        This test will check the functioning of the bonus award to a
+        worker for an assignment
+        """
+        startTime = timezone.now()
+        self.create_quals()
+        self.create_workers()
+        requester = self.create_new_client("req1")
+
+        maxAssigns = 1
+        lifetime = 10000
+        assignDur = 1000
+        reward = "0.13"
+        title = "Create Task with Quals"
+        desc = "Little bit of sugar"
+        annot = "asdf"
+        question = load_quesform(2)
+
+        resp = self.client.create_hit(
+            MaxAssignments = maxAssigns,
+            LifetimeInSeconds = lifetime,
+            AssignmentDurationInSeconds = assignDur,
+            Reward = reward,
+            Title = title,
+            Description = desc,
+            Question = question,
+            RequesterAnnotation = annot,
+            UniqueRequestToken="ureuw"
+        )
+        self.is_ok(resp)
+
+        taskId = resp["HIT"]["HITId"]
+        task = Task.objects.get(aws_id = taskId)
+
+        assignment = self.actors[0].accept_task(task)
+
+        # Check that the bonus function can't be used here
+        RequestError = self.client._load_exceptions().RequestError
+        with self.assertRaises(RequestError):
+            resp = self.client.send_bonus(
+                WorkerId = self.actors[0].worker.aws_id,
+                BonusAmount = "1.00",
+                AssignmentId = assignment.aws_id,
+                Reason = "What a good job",
+            )
+
+        with self.assertRaises(RequestError):
+            resp = requester.send_bonus(
+                WorkerId = self.actors[0].worker.aws_id,
+                BonusAmount = "1.00",
+                AssignmentId = assignment.aws_id,
+                Reason = "What a good job",
+            )
+
+        numBonuses = self.actors[0].worker.bonuspayment_set.count()
+        self.assertEqual( numBonuses, 0 )
+
+
+        data = {
+            "favorite" : ["blue"],
+            "acceptible" : ["red", "blue"]
+        }
+        self.actors[0].complete_assignment( assignment, data )
+
+        # Assignment has been submitted and ready for review
+
+        # Bonus can be awarded at this stage - so cool=
+        resp = self.client.send_bonus(
+            WorkerId = self.actors[0].worker.aws_id,
+            BonusAmount = "1.00",
+            AssignmentId = assignment.aws_id,
+            Reason = "What a good job",
+        )
+        self.is_ok(resp)
+
+        numBonuses = self.actors[0].worker.bonuspayment_set.count()
+        self.assertEqual( numBonuses, 1 )
+
+        # Check that the bonus function can't be used by other
+        # Requester
+        with self.assertRaises(RequestError):
+            resp = requester.send_bonus(
+                WorkerId = self.actors[0].worker.aws_id,
+                BonusAmount = "1.00",
+                AssignmentId = assignment.aws_id,
+                Reason = "What a good job",
+            )
+
+        # Requester Approves the assignment
+        resp = self.client.approve_assignment(
+            AssignmentId = assignment.aws_id
+        )
+        self.is_ok(resp)
+
+        # We should now be able to send a bonus to the worker
+        resp = self.client.send_bonus(
+            WorkerId = self.actors[0].worker.aws_id,
+            BonusAmount = "1.00",
+            AssignmentId = assignment.aws_id,
+            Reason = "What a good job",
+        )
+        self.is_ok(resp)
+
+        numBonuses = self.actors[0].worker.bonuspayment_set.count()
+        self.assertEqual( numBonuses, 2 )
+
+        # Check that another requester can't send a bonus
+        with self.assertRaises(RequestError):
+            resp = requester.send_bonus(
+                WorkerId = self.actors[0].worker.aws_id,
+                BonusAmount = "1.00",
+                AssignmentId = assignment.aws_id,
+                Reason = "What a good job",
+            )
+
+        # Check for send bonus on a worker/assignment
+        # mismatch.
+        with self.assertRaises(RequestError):
+            resp = self.client.send_bonus(
+                WorkerId = self.actors[1].worker.aws_id,
+                BonusAmount = "1.00",
+                AssignmentId = assignment.aws_id,
+                Reason = "What a good job",
+            )
+
+
+    def test_worker_with_block(self):
+        """
+        Test that an assignment can't be accepted by a worker with
+        a block
+        """
+        pass
